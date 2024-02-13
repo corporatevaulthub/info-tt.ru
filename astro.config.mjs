4ba52463c8a8590671f565964b23e127e97f7606
artyom yarmoluk <a.s.yarmoluk@gmail.com>
2024-02-13 14:27:36 +0300

    analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,11 +11,13 @@ import vercel from "@astrojs/vercel/serverless";
 export default defineConfig({
   site: "https://info-tt.ru",
   output: "hybrid",
+
   adapter: vercel({
     webAnalytics: {
       enabled: true,
     },
   }),
+
   integrations: [
     tailwind({
       applyBaseStyles: false,
