e14dd5274f215f20afa6bd719547f81aa88f88a8
artyom yarmoluk <a.s.yarmoluk@gmail.com>
2024-02-21 14:40:19 +0300

    b24 lead added

--- src/components/SaleForm.tsx
+++ src/components/SaleForm.tsx
@@ -29,6 +29,9 @@ const SaleForm = () => {
             TITLE: "Новый лид с формы (Акция)",
             NAME: formData.name,
             PHONE: [{ VALUE: phone, VALUE_TYPE: "WORK" }],
+            COMMENTS: `
+              Комментарий: ${formData.comment},
+            `,
           },
         };
 
@@ -74,13 +77,24 @@ const SaleForm = () => {
 
   return (
     <form ref={form} onSubmit={handleSubmit(sendEmail)}>
-      <fieldset className="mb-4 flex w-full max-w-[540px] flex-col items-start gap-2 lg:max-w-full lg:flex-row">
-        <input required className={`input h-[43px] w-full lg:w-[260px] ${errors.name && "border-2 border-red-500"}`} placeholder="Имя" {...register("name", { required: true })} />
-        <IMaskInput required className="input h-[43px] w-full lg:w-[260px]" mask="+{7}(000)000-00-00" name="phone" placeholder="Телефон" inputRef={phoneInputRef} />
-        <button className="button h-[43px] w-full justify-center  bg-[#E53737] px-[43px] disabled:pointer-events-none disabled:bg-dark sm:w-auto" disabled={loading}>
-          {loading ? "Отправка.." : "Отправить"}
-        </button>
+      <fieldset className="mb-4 flex w-full max-w-[540px] flex-wrap items-start gap-2 lg:max-w-full">
+        <div className="flex w-full flex-col gap-2 lg:w-auto lg:flex-row">
+          <input required className={`input h-[43px] w-full lg:w-[260px] ${errors.name && "border-2 border-red-500"}`} placeholder="Имя" {...register("name", { required: true })} />
+          <IMaskInput required className="input h-[43px] w-full lg:w-[260px]" mask="+{7}(000)000-00-00" name="phone" placeholder="Телефон" inputRef={phoneInputRef} />
+        </div>
+        <div className="flex w-full flex-col gap-2 lg:w-auto lg:flex-row">
+          <input
+            className={`input h-[43px] w-full lg:w-[530px] ${errors.comment && "border-2 border-red-500"}`}
+            placeholder="Комментарий"
+            {...register("comment", { required: true })}
+            style={{ verticalAlign: "bottom" }}
+          />
+          <button className="button h-[43px] w-full justify-center bg-[#E53737] px-[43px] disabled:pointer-events-none disabled:bg-dark lg:w-[260px]" disabled={loading}>
+            {loading ? "Отправка.." : "Отправить"}
+          </button>
+        </div>
       </fieldset>
+
       <label className="flex cursor-pointer items-center gap-2 text-[#2E3037]">
         <input required type="checkbox" {...register("accept", { required: true })} className="form-checkbox rounded border-[#D5DAE3] text-primary  focus:ring-0" />
         <span className="text-[12px] font-semibold sm:text-sm">Я согласен с политикой конфиденциальности</span>
